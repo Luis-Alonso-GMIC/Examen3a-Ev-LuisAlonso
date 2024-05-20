a860632263efbc69b4332ef29268d05748049f94
Luis-AlonsoASIR <123066120+Luis-Alonso-GMIC@users.noreply.github.com>
2024-05-20 09:42:33 +0200

    Examen

---
+++ script.js
@@ -0,0 +1,11 @@
+let countEl = document.getElementById("count-el");
+document.getElementById('myButton')
+let numero = 5;
+
+function aprobar() {
+
+    countEl.textContent = numero
+    console.log(numero);
+    alert("¡¡¡has aprobado!!!");
+    
+}
